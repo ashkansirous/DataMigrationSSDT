9ca14b1109fb885308ef5dcf1876085d7b7406c5
Ashkan Sirous <asi@templafy.com>
2019-07-22 15:01:43 +0200

    init

---
+++ DatabaseProject/Tenants.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tenants]
+(
+	[Id] VARCHAR (20)   NOT NULL, 
+    [Name]                  NVARCHAR (50)   NOT NULL,
+    [Partner]               NVARCHAR (100)  NULL,
+CONSTRAINT [PK_Tenants] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
